14e3d4a602cbc4c995d5a1880956f81e38f9ba83
corantoine <158170889+corantoine@users.noreply.github.com>
2024-02-08 11:30:50 +0100

    exercices 1 et 2 faits

--- chapter01/exercice01/index.js
+++ chapter01/exercice01/index.js
@@ -1,11 +1,15 @@
 
 export function greetWorld() {
+    return "Hey, world!"
 
 }
 
 export function greet(name) {
+    return "hey, "+name+"!";
 }
 
-export function capitilizedGreet(name) {
 
+export function capitilizedGreet(name) {
+ let capitilizedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+ return "Hey, " + capitilizedName+"!"
 }
--- chapter01/exercice02/index.js
+++ chapter01/exercice02/index.js
@@ -1,8 +1,12 @@
 export function sum(a, b) {
+    return a+b;
 }
 
 export function isGreaterThan(input, comp) {
+   return input > comp ; 
 }
 
 export function reverseNumber(num) {
+    return num .toString().split('').reverse().join('');
+    
 }
