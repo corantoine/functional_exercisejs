a804524f0a16d9d95b4d1bf3bc35f0732d887c89
corantoine <158170889+corantoine@users.noreply.github.com>
2024-02-08 13:53:58 +0100

    ajout exercice 3

--- chapter01/exercice03/index.js
+++ chapter01/exercice03/index.js
@@ -1,8 +1,17 @@
 export function reverseString(str) {
+    return str .toString().split('').reverse().join('');
 }
 
 export function isPalindrome(str) {
+let str2 = str.toString().split('').reverse().join('')
+    if (str==str2){
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 export function countVowels(str) {
+    return str.match(/[aeiou]/gi).length;
 }
